e48c5eac0fd41d7f160a6700984da86377a18e82
manasi22cs090-arch <manasi.22cs090@sode-edu.in>
2025-09-08 15:15:14 +0530

    Update and rename pushbutton.ino to lab2.ino

---
+++ lab2.ino
@@ -0,0 +1,17 @@
+int buttonState = 1;
+
+void setup()
+{
+  pinMode(9, INPUT);
+  pinMode(3, OUTPUT);
+}
+
+void loop()
+{
+  buttonState = digitalRead(9);
+  if (buttonState == 1) {
+    digitalWrite(3, HIGH);
+  } else {
+    digitalWrite(3 , LOW);
+  }
+}
